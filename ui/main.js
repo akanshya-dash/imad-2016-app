a8b13f0bc0a9f59e46707e582b3e6e52ff250d79
akanshya-dash <heartcore.9z12.hhh@gmail.com>
2016-10-01 23:47:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,16 @@
 console.log('Loaded!');
 //move! moving!!
 var img = document.getElementById('madi');
-madi.onclick = function(){
+var marginLeft = 0;
+function moveRight(){
+ 
+ marginleft= marginLeft + 10;
+ img.style.marginLeft = marginleft + 'px';
+ 
+}
+madi.onclick = function()
+{
+    
   img.style.marginLeft = '100px';  
+  var interval = setInterval(moveRight, 100);
 };
