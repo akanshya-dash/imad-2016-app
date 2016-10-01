651763c9767574ad1571d9516b1a143a21df819c
akanshya-dash <heartcore.9z12.hhh@gmail.com>
2016-10-01 23:16:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ var aricles ={
     date: 'september of twenty seventh, two thousand and sixteen',
     content: ` <p>
             
-            this is a article writtten by me. it is about the fantasy novel im working upon. its a breif introduction to the novel scenario. hope you enjoy it.
-            the story is set in a royal setup. it begins witha huge destruction of the most powerful kingdom. and the queen and her children fleeing the kingdom leaving the king behind, who is actually, cursed.
+            hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. 
             </p> `
         },
            'article-2':{ 
@@ -22,7 +21,7 @@ var aricles ={
     date: 'september of twenty seventh, two thousand and sixteen',
     content: ` <p>
             
-           hello this is akanshya. i am an undergraduate student doing btech in computer science, at srm university. my university sucks. but still we all manage to get through it. i hope i can make an appeal but here the warden of one of the hostels strongle discriminates betwen northie and southie ppl. she tries to hummillate us and show us down. south indianas must be assasianated.
+           hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. 
             </p> `
                
            },
@@ -32,7 +31,7 @@ var aricles ={
     date: 'september of twenty seventh, two thousand and sixteen',
     content: ` <p>
             
-            i totally hate my roomate. she is slum girl. i wanna run away. i wanna stay with my parents i want freedom. i want my personal life to be my personal life not something that is shared with every bengoli parent of this university. i hate my warden. i hate one lady in my department. i despise two girls in this place. and i hate being alone. so basically, i need help.
+            hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. hello this is akanshya. 
             </p> `
         
            
