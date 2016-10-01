d48f5c93e3e4c2d51685c730441163cbd2f32727
akanshya-dash <heartcore.9z12.hhh@gmail.com>
2016-10-01 22:31:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ var aricles ={
         }            
         
 
-app.get('/', function (req, res) {
+app.get('/ui/index.html', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
